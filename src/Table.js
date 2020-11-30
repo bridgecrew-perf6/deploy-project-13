2d1a2043ecb3d314816cfb4771fe2b476d0f2261
Ricardo Maya <rickmaya@gmail.com>
2020-11-30 12:02:53 -0600

    moved data from Tablebody component to the render in the App component then passed characterData to the Table component as props

--- src/Table.js
+++ src/Table.js
@@ -12,34 +12,16 @@ const TableHeader = () => {
 }
 
 const Tablebody = () => {
-  return (
-    <tbody>
-      <tr>
-        <td>Charlie</td>
-        <td>Janitor</td>
-      </tr>
-      <tr>
-        <td>Mac</td>
-        <td>Bouncer</td>
-      </tr>
-      <tr>
-        <td>Dee</td>
-        <td>Aspiring actress</td>
-      </tr>
-      <tr>
-        <td>Dennis</td>
-        <td>Bartender</td>
-      </tr>
-    </tbody>
-  )
+  return <tbody />
 }
 
 class Table extends Component {
   render() {
+    const {characterData} = this.props
     return (
       <table>
         <TableHeader />
-        <Tablebody />
+        <Tablebody characterData={characterData}/>
       </table>
     )
   }
